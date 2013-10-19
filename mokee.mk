b5009c2aa1e9742399ff312c7f085482047b0cb7
martincz <martincz@mokeetek.com>
2013-10-20 00:43:00 +0800

    mokee modify
    Change-Id: I3c115e2a6036f38effe4f39b91bcd811d0703818

--- mokee.mk
+++ mokee.mk
@@ -1,11 +1,11 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/mk/config/cdma.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -13,7 +13,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := DROID RAZR HD
-PRODUCT_NAME := cm_xt926
+PRODUCT_NAME := mk_xt926
 
 $(call inherit-product, device/motorola/xt926/full_xt926.mk)
 
